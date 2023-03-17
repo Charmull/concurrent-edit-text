8918fbb753ff434cfdae43732c48cfd70dfa8210
Charmull <527wlals@gmail.com>
2023-03-17 13:36:18 +0900

    refactor: folder tree

---
+++ concurrent-edit-text/src/App.js
@@ -0,0 +1,58 @@
+import logo from "./logo.svg";
+import "./App.css";
+import yorkie from "yorkie-js-sdk";
+import { useEffect } from "react";
+
+function App() {
+  const client = new yorkie.Client("https://api.yorkie.dev", {
+    apiKey: "cg96lvi1i6k1erovlodg",
+  });
+
+  const doc = new yorkie.Document("concorrent-edit-text");
+
+  let editor = document.getElementById("editor");
+
+  const main = async () => {
+    await client.activate();
+
+    await client.attach(doc);
+
+    doc.update(root => {
+      // console.log(root.text);
+      if (root.text) {
+        editor.value = root.text;
+      } else {
+        root.text = "Edit";
+      }
+    });
+
+    doc.subscribe(event => {
+      if (event.type === "remote-change") {
+        editor.value = doc.getRoot().text;
+      }
+    });
+  };
+
+  const editorHandler = event => {
+    doc.update(root => {
+      root.text = event.target.value;
+    });
+  };
+
+  useEffect(() => {
+    editor = document.getElementById("editor");
+  }, []);
+
+  useEffect(() => {
+    editor && main();
+  }, [editor]);
+
+  return (
+    <div className="App">
+      <input id="editor" onInput={editorHandler}></input>
+      <canvas></canvas>
+    </div>
+  );
+}
+
+export default App;
