11c21e968df7d8fd12aa8e36cbdbba2d8cc0190c
Charmull <527wlals@gmail.com>
2023-03-17 13:49:40 +0900

    project2 setting

---
+++ concurrent-edit-codeEditor/concurrent-edit-codeeditor/src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div id="network-status"></div>
+      <div id="editor"></div>
+      <div id="peers"></div>
+      <div id="document"></div>
+      <div id="document-text"></div>
+    </div>
+  );
+}
+
+export default App;
